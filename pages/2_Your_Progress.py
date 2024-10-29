ec3704dcb642fc46a2d69f269778a3f9a4a8b067
anhn <anhnd85@gmail.com>
2024-10-29 21:02:30 +0100

    Update 2_Your_Progress.py

--- pages/2_Your_Progress.py
+++ pages/2_Your_Progress.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from st_pages import add_indentation, hide_pages,show_pages_from_config
 import extra_streamlit_components as stx
 import time
 from streamlit_extras.switch_page_button import switch_page
