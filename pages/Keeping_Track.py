cfcbc4c371df71259e1f0f040fb4a0edd20db1b1
anhn <anhnd85@gmail.com>
2025-01-02 21:59:17 +0100

    Update Keeping_Track.py

--- pages/Keeping_Track.py
+++ pages/Keeping_Track.py
@@ -190,17 +190,17 @@ with st.form("this"):
     st.write("4. Scope and Time Management")
     wbs = st.selectbox(
         "WBS",
-        options=['WBS is created', 'WBS is validated'],
-        index=['CreateWBS', 'ValidateWBS'].index(
-            st.session_state["retrieved_data"].get("WBS", "CreateWBS")
+        options=['Not done', 'WBS is created', 'WBS is validated'],
+        index=['CreateNoWBS', 'CreateWBS', 'ValidateWBS'].index(
+            st.session_state["retrieved_data"].get("WBS", "CreateNoWBS")
         )
     )  
     time_estimated = st.checkbox("Time Estimated for Activities ?", value=st.session_state["retrieved_data"].get("TimeEstimated", False))
     ganttchart = st.selectbox(
         "GanttChart",
-        options=['Gantt Chart is created', 'Gantt Chart is validated'],
-        index=['CreateGantt', 'ValidateGantt'].index(
-            st.session_state["retrieved_data"].get("GanttChart", "CreateGantt")
+        options=['Not done', 'Gantt Chart is created', 'Gantt Chart is validated'],
+        index=['CreateNoGantt', 'CreateGantt', 'ValidateGantt'].index(
+            st.session_state["retrieved_data"].get("GanttChart", "CreateNoGantt")
         )
     )      
     st.write("5. Risk and Communication Management")
